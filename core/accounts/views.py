69af8e69f796317a8bb0781a18416927f63764db
Lucasshaw12 <shaw51@hotmail.co.uk>
2026-01-06 18:57:35 +0000

    redirect

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -5,5 +5,5 @@ from .forms import CustomUserCreationForm
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('ticket_list')
+    success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
